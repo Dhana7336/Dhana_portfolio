93dbbabb8fc84d48059f125e9683f7b36216832b
parupudi Dhana Lakshmi <parupudidhana@parupudis-MacBook-Air.local>
2025-09-09 22:02:34 -0400

    Updates

---
+++ precache-manifest.8327488f210ba49a9d74f9892b85761f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323e44473787995dc3a85c260278d9dc",
+    "url": "/Dhana_portfolio/index.html"
+  },
+  {
+    "revision": "df12685dcb388ab846b1",
+    "url": "/Dhana_portfolio/static/css/2.c03e993c.chunk.css"
+  },
+  {
+    "revision": "ef8115bc817a1be190e0",
+    "url": "/Dhana_portfolio/static/css/main.8678fb65.chunk.css"
+  },
+  {
+    "revision": "df12685dcb388ab846b1",
+    "url": "/Dhana_portfolio/static/js/2.65018e77.chunk.js"
+  },
+  {
+    "revision": "e9d9b058df1e97b30148d7042082faa1",
+    "url": "/Dhana_portfolio/static/js/2.65018e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8115bc817a1be190e0",
+    "url": "/Dhana_portfolio/static/js/main.36ed286c.chunk.js"
+  },
+  {
+    "revision": "7f2c905b076eded1f4af",
+    "url": "/Dhana_portfolio/static/js/runtime-main.82a4d937.js"
+  },
+  {
+    "revision": "fb291490ea2fd02c2aa7f0b472d4ce3e",
+    "url": "/Dhana_portfolio/static/media/myProfile.fb291490.png"
+  }
+]);
